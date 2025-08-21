9ae47385cba44c7f07fef26d1ed4db87481be5b6
CaioDalNegro <caiodalnegro@gmail.com>
2025-08-21 19:33:59 -0300

    Subindo estruturas;

---
+++ backend/src/main/java/br/com/pateandoapp/pateandobackend/PateandobackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.pateandoapp.pateandobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PateandobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PateandobackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/pateandoapp/pateandobackend/PateandobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pateandoapp.pateandobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PateandobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
