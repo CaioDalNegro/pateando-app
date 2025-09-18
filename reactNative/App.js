4a1de003e966aafe2f8b9514e346c0fed351227e
CaioDalNegro <caiodalnegro@gmail.com>
2025-09-18 19:56:32 -0300

    NEW: alteração app.js

--- reactNative/App.js
+++ reactNative/App.js
@@ -1,8 +1,7 @@
 // App.js
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
-import RegisterScreen from './src/screens/RegisterScreen';
 
 export default function App() {
-  return <RegisterScreen />;
+  return <AppNavigator />;
 }
