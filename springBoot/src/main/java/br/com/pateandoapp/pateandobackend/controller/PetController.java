08dfea15f979ae42b30c2a5939381e897f1afd9d
caiodalnegro@gmail.com <caiodalnegro@gmail.com>
2025-10-09 19:59:21 -0300

    NEW: criando post de pets

--- springBoot/src/main/java/br/com/pateandoapp/pateandobackend/controller/PetController.java
+++ springBoot/src/main/java/br/com/pateandoapp/pateandobackend/controller/PetController.java
@@ -1,17 +1,32 @@
 package br.com.pateandoapp.pateandobackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.pateandoapp.pateandobackend.repository.PetRepository;
-import br.com.pateandoapp.pateandobackend.repository.UsuarioRepository;
+import br.com.pateandoapp.pateandobackend.model.Pet;
+import br.com.pateandoapp.pateandobackend.service.PetService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 /**
  * Controller responsável pelos endpoints de Pets.
  */
 @RestController
 @RequestMapping("/pets")
+@CrossOrigin(origins = "*") // Permite requisições do app React Native
 public class PetController {
     
+    @Autowired
+    private PetService petService;
+    
+    @PostMapping("/create/{usuarioId}")
+    public Pet createPet(@PathVariable Long usuarioId, @RequestBody Pet pet) {
+        return petService.createPet(usuarioId, pet)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
 }
--- springBoot/src/main/java/br/com/pateandoapp/pateandobackend/service/PetService.java
+++ springBoot/src/main/java/br/com/pateandoapp/pateandobackend/service/PetService.java
@@ -1,11 +1,30 @@
 package br.com.pateandoapp.pateandobackend.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.pateandoapp.pateandobackend.model.Pet;
+import br.com.pateandoapp.pateandobackend.repository.PetRepository;
+import br.com.pateandoapp.pateandobackend.repository.UsuarioRepository;
 
 /**
  * Classe de serviço responsável pelas regras de negócio relacionadas aos Pets.
  */
 @Service
 public class PetService {
-    
+
+    @Autowired
+    private PetRepository petRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Optional<Pet> createPet(Long usuarioId, Pet pet) {
+        return usuarioRepository.findById(usuarioId).map(usuario -> {
+            pet.setDono(usuario);
+            return petRepository.save(pet);
+        });
+    }    
 }
